cb4412cd78da9b9938f85b58416edeb75c57d497
RohanBhalla <rohan.bhalla@curveai.io>
2024-12-12 22:33:08 +0530

    changed file layout

--- backend/tagalong-api/tagalong_api/helpers/enable_cors.py
+++
@@ -1,9 +0,0 @@
-from tg import response
-
-def enable_cors():
-    """
-    Set CORS headers in the response.
-    """
-    response.headers['Access-Control-Allow-Origin'] = '*'  # This allows any origin. In a real-world scenario, you'd probably limit this.
-    response.headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, DELETE, OPTIONS'
-    response.headers['Access-Control-Allow-Headers'] = 'Origin, Accept, Content-Type, X-Requested-With, X-CSRF-Token, Authorization'
